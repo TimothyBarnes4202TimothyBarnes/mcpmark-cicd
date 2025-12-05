adad3cf066392744e2f62d128d62b3cfd8dc5e72
TimothyBarnes4202TimothyBarnes <grumpy-barns9o@icloud.com>
2025-12-05 18:03:25 +0800

    Add ESLint config, workflow, and example file with lint errors

---
+++ src/example.js
@@ -0,0 +1,7 @@
+// This file has intentional linting errors to test the workflow
+const unusedVariable = 'test';
+console.log("Hello, world!");
+function testFunction() {
+  let value = 10
+  return value
+}
